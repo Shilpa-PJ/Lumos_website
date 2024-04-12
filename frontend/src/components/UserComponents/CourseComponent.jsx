ac94445f5e3785d8e2009474c0be5be01091be8b
Shilpa-PJ <shilpapj2000@gmail.com>
2024-04-12 16:49:39 +0530

    added chatbot

--- frontend/src/components/UserComponents/CourseComponent.jsx
+++ frontend/src/components/UserComponents/CourseComponent.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Box, Heading, Stack, Wrap, WrapItem } from "@chakra-ui/react";
 import LandingPageCarousel from "../../Pages/LandingPageComponents/LandingPageCarousel";
 import InProgressCarousel from "./InProgressCarousel";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRobot } from '@fortawesome/free-solid-svg-icons';
 // import LandingPageCarousel from '../'
 const CourseComponent = () => {
   return (
@@ -19,6 +21,10 @@ const CourseComponent = () => {
         <Heading as="h2" size="lg">
           In Progress Courses
         </Heading>
+        <a href="http://localhost:8501/" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faRobot} style={{ marginRight: '5px' }} />
+      <b>Ask question to ChatBot</b>
+    </a>
         <Wrap spacing={4}>
           <InProgressCarousel />
         </Wrap>
